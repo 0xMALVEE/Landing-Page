0ad9e976b88c6e227008a6defc5ad0d0f57e4796
Alvee <M.Alvee8141@gmail.com>
2018-05-31 16:59:38 +0600

    Final Comit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var typed = new Typed('.typed', {
+        strings: ["Welcome to RoKu", "I am web developer", "Make web app and sites", "Want to work with me ?"],
+        typeSpeed: 100,
+        loop:true
+      });
+});
